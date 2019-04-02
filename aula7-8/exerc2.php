de8cb59f8afdf7204605a554eeb04391e5390d8e
dani <xpert_551@hotmail.com>
2019-04-02 21:04:32 +0100

    update

---
+++ aula7-8/exerc2.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>PHP</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	<script type="text/javascript" src="scripts/script.js" async></script>
+</head>
+<body>
+
+<?php
+$a = hello;
+$b = World;
+echo a.b;
+?>
+	
+
+</body>
+</html>
